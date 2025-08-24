2463786030c3a22bd8102456b735b63c502ee1aa
NilsCraftHD <97766170+nilscrafthd@users.noreply.github.com>
2025-08-24 16:48:10 +0200

    Update repos.jsx

--- pages/api/util/repos.jsx
+++ pages/api/util/repos.jsx
@@ -4,8 +4,8 @@ export default async function Repos(req, res) {
     res.json(
         await (await fetch('https://api.github.com/users/nilscrafthd/repos', {
             headers: {
-                'Authorization': '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'
-            } // get token: https://github.com/settings/tokens
+                'Authorization': `token ${process.env.GITHUB_TOKEN}`
+            }
         })).json()
     );
 };
